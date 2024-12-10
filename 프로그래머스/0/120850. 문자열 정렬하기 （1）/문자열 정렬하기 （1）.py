b41c942d9cf67c865433effc61bc5507926dc3d4
yammyam <96424434+yammyam@users.noreply.github.com>
2024-12-11 00:51:33 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
@@ -2,8 +2,8 @@ def solution(my_string):
     answer = []
     for i in my_string:
         if i in "0123456789":
+            # 해당 문자열이 isdigit()함수로 숫자인지 판별가능
             answer.append(i)
     for i in range(0,len(answer)):
         answer[i] = int(answer[i])
-    answer.sort()
-    return answer
+    return sorted(answer)
