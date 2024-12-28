e4737c8a3348bbdc7233456d575e08855614b118
yammyam <96424434+yammyam@users.noreply.github.com>
2024-12-29 04:35:57 +0900

    [level 0] Title: 가까운 수, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/가까운 수.py
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.py
@@ -6,14 +6,16 @@
 #             value = abs(n-i)
 #             answer = index
 #     return array[answer]
+
+# 위의 코드 안되는 점 [23,21] 22 일때 결괏값 21이 도출이 안됨
 def solution(array, n):
     answer = 0
-    value = 101  # 초기값 설정
+    value = 101
     for index, i in enumerate(array):
-        if abs(n - i) < value:  # 더 가까운 수를 찾은 경우
-            value = abs(n - i)
+        if abs(n-i) < value:
+            value = abs(n-i)
             answer = index
-        elif abs(n - i) == value:  # 거리가 같은 경우 더 작은 수 선택
-            if i < array[answer]:
+        elif abs(n-i) == value:
+            if i<array[answer]:
                 answer = index
     return array[answer]
