5cff69033d6e98e7912b67072b52925416d76e88
yammyam <96424434+yammyam@users.noreply.github.com>
2024-12-30 03:22:05 +0900

    [level 0] Title: 가까운 수, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/가까운 수.py
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.py
@@ -10,21 +10,25 @@
 
 # 위의 코드 안되는 점 [23,21] 22 일때 결괏값 21이 도출이 안됨
 
-2.
-def solution(array, n):
-    answer = 0
-    value = 101
-    for index, i in enumerate(array):
-        if abs(n-i) < value:
-            value = abs(n-i)
-            answer = index
-        elif abs(n-i) == value:
-            if i<array[answer]:
-                answer = index
-    return array[answer]
+# 2.
+# def solution(array, n):
+#     answer = 0
+#     value = 101
+#     for index, i in enumerate(array):
+#         if abs(n-i) < value:
+#             value = abs(n-i)
+#             answer = index
+#         elif abs(n-i) == value:
+#             if i<array[answer]:
+#                 answer = index
+#     return array[answer]
 
 # 3.
 # 선정렬
 # 답이 배열 안에 있는가 ?  -> 어레이섞어도될듯?
 # [22,24] 22   ,  [24,22] 22
+def solution(array, n):
+    array.sort()
+    answer = list(map(lambda x : abs(n-x),array))
+    return array[answer.index(min(answer))]
 
