2c22892e891ab9f6708a491961937884474d94ba
yammyam <96424434+yammyam@users.noreply.github.com>
2025-01-08 23:45:58 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.py
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.py
@@ -1,5 +1,8 @@
+# def solution(strlist):
+#     answer = [];
+#     for i in strlist:
+#         answer.append(len(i));
+#     return answer
+# map으로 한줄 
 def solution(strlist):
-    answer = [];
-    for i in strlist:
-        answer.append(len(i));
-    return answer
+    return list(map(lambda x: len(x),strlist))
