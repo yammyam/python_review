ac3232e001822199260002e3ad16e7305af6e563
yammyam <96424434+yammyam@users.noreply.github.com>
2025-01-14 23:18:12 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.py
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.py
@@ -1,9 +1,3 @@
-# def solution(my_string):
-#     answer = ''
-#     for i in my_string:
-#         if i not in answer:
-#             answer += i
-#     return answer
 def solution(my_string):
     return "".join(dict.fromkeys(my_string))
 # 딕셔너리에 넣어 중복된거 자체제거
