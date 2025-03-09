2345c29c8f06dfbc42417d971aa6ba6dc7577807
yammyam <96424434+yammyam@users.noreply.github.com>
2025-03-09 23:47:26 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120924. 다음에 올 숫자/README.md
+++ 프로그래머스/0/120924. 다음에 올 숫자/README.md
@@ -1,6 +1,6 @@
 # [level 0] 다음에 올 숫자 - 120924 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120924?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120924) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 23일 23:39:40
+2025년 03월 09일 23:47:24
 
 ### 문제 설명
 
--- 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.py
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.py
@@ -1,7 +1,9 @@
+# def solution(common):
+#     # 등차수열인지 확인
+#     same_minus = common[1] - common[0] == common[2] - common[1]
+#     if same_minus:
+#         return common[-1] + (common[1] - common[0])
+#     else:
+#         return common[-1] * (common[1] / common[0])
 def solution(common):
-    # 등차수열인지 확인
-    same_minus = common[1] - common[0] == common[2] - common[1]
-    if same_minus:
-        return common[-1] + (common[1] - common[0])
-    else:
-        return common[-1] * (common[1] / common[0])
+    return common[-1] + (common[1] - common[0]) if common[1] - common[0] == common[2] - common[1] else common[-1] * (common[1] / common[0])
