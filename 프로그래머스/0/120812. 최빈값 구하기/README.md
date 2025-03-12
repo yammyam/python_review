da424225d9b5c53e82081be9fc05312888292e4a
yammyam <96424434+yammyam@users.noreply.github.com>
2025-03-12 23:57:36 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.06 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/README.md
+++ 프로그래머스/0/120812. 최빈값 구하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 최빈값 구하기 - 120812 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120812?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120812) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.15 ms
+메모리: 9.11 MB, 시간: 0.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 14일 00:03:17
+2025년 03월 12일 23:57:33
 
 ### 문제 설명
 
--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
@@ -1,18 +1,7 @@
+from collections import Counter
+
 def solution(array):
-    res = [0] * 1000
-    
-    for num in array:
-        res[num] += 1
-    
-    max_val = max(res)
-    count = 0
-    answer = -1
-    
-    for i, val in enumerate(res):
-        if val == max_val:
-            answer = i
-            count += 1
-        if count >= 2:
-            return -1
-    
-    return answer
+    count = Counter(array)
+    max_val = max(count.values())
+    mode = [k for k, v in count.items() if v == max_val]
+    return mode[0] if len(mode) == 1 else -1
