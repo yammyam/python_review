1a38069d4c3a4c1924a81c7316e9d76e3c32378e
yammyam <96424434+yammyam@users.noreply.github.com>
2025-05-12 23:58:29 +0900

    [level 1] Title: 최소직사각형, Time: 1.83 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -0,0 +1,15 @@
+def solution(sizes):
+    a = 0;
+    b = 0;
+    for i in range(len(sizes)):
+        sizes[i].sort()
+    for i in sizes:
+        if(a==0):
+            a = i[0]
+        if(b == 0):
+            b = i[1]
+        if(a < i[0]):
+            a = i[0]
+        if(b< i[1]):
+            b = i[1]
+    return a*b;
